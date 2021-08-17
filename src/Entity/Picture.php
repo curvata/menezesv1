55457131256a789d0c2c92b0c0b76670876c55f3
Cancelinha Menezes Mike <cancelinha.menezes@gmail.com>
2021-08-17 19:30:36 +0200

    Création des entités et des bases de données de production et de test

---
+++ src/Entity/Picture.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PictureRepository;
+use DateTime;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PictureRepository::class)
+ */
+class Picture
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private DateTime $createdAt;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private string $filename;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Project::class, inversedBy="pictures")
+     */
+    private Project $project;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getFilename(): ?string
+    {
+        return $this->filename;
+    }
+
+    public function setFilename(string $filename): self
+    {
+        $this->filename = $filename;
+
+        return $this;
+    }
+
+    public function getProject(): ?Project
+    {
+        return $this->project;
+    }
+
+    public function setProject(?Project $project): self
+    {
+        $this->project = $project;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Project.php
@@ -0,0 +1,199 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProjectRepository;
+use DateTime;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+
+/**
+ * @ORM\Entity(repositoryClass=ProjectRepository::class)
+ */
+class Project
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private DateTime $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private DateTime $updatedAt;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private string $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private string $description;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private string $linkWeb;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private string $linkGithub;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private string $headerImage;
+
+    private File $headerImageFile;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Picture::class, mappedBy="project")
+     */
+    private $pictures;
+
+    public function __construct()
+    {
+        $this->pictures = new ArrayCollection();
+    }
+
+    public function getHeaderImageFile(): File
+    {
+        return $this->headerImageFile;
+    }
+
+    public function __toString(): string
+    {
+        return $this->title;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCreatedAt(): ?DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(DateTime $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?DateTime $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getLinkWeb(): ?string
+    {
+        return $this->linkWeb;
+    }
+
+    public function setLinkWeb(?string $linkWeb): self
+    {
+        $this->linkWeb = $linkWeb;
+
+        return $this;
+    }
+
+    public function getLinkGithub(): ?string
+    {
+        return $this->linkGithub;
+    }
+
+    public function setLinkGithub(?string $linkGithub): self
+    {
+        $this->linkGithub = $linkGithub;
+
+        return $this;
+    }
+
+    public function getHeaderImage(): ?string
+    {
+        return $this->headerImage;
+    }
+
+    public function setHeaderImage(string $headerImage): self
+    {
+        $this->headerImage = $headerImage;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Picture[]
+     */
+    public function getPictures(): Collection
+    {
+        return $this->pictures;
+    }
+
+    public function addPicture(Picture $picture): self
+    {
+        if (!$this->pictures->contains($picture)) {
+            $this->pictures[] = $picture;
+            $picture->setProject($this);
+        }
+
+        return $this;
+    }
+
+    public function removePicture(Picture $picture): self
+    {
+        if ($this->pictures->removeElement($picture)) {
+            // set the owning side to null (unless already changed)
+            if ($picture->getProject() === $this) {
+                $picture->setProject(null);
+            }
+        }
+
+        return $this;
+    }
+}
